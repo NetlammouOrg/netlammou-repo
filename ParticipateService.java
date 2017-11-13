6a2696056d6bbfd5252d5d93d2cb60ce0e9eda41
ikrambk <33398086+ikrambk@users.noreply.github.com>
2017-11-13 23:58:57 +0100

    Add files via upload
    ikram commit 1.1

---
+++ ParticipateService.java
@@ -0,0 +1,29 @@
+package tn.esprit.pidev.services;
+
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import tn.esprit.pidev.persistence.Participate;
+import tn.esprit.pidev.util.ParticipateServiceRemote;
+
+@Stateless
+@LocalBean
+public class ParticipateService implements ParticipateServiceRemote {
+	
+	
+	@PersistenceContext(unitName = "pidev-ejb")
+	EntityManager em;
+
+	@Override
+	public void ajouterParticipate(Participate participate) {
+		em.persist(participate);
+	
+	}
+
+
+
+
+}
