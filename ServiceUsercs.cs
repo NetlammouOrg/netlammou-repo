77b53fb3920aa169b8f4034472c23e0d2a4b7df3
hajerh <hajer.benali1@esprit.tn>
2017-12-12 09:36:40 +0100

    Add files via upload

---
+++ ServiceUsercs.cs
@@ -0,0 +1,21 @@
+﻿using Data.Infrastructure;
+using Domain.Entity;
+using Service.Pattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ServiceUsercs:Service<user>,IUserService
+    {
+        private static DatabaseFactory dbf = new DatabaseFactory();
+        private static UnitOfWork uwk = new UnitOfWork(dbf);
+        public ServiceUsercs(): base(uwk)
+        {
+        }
+
+    }
+}
